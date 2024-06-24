f6fb6f0079027b640b7c8a4f45f2d84f32994102
Akash Morwal <akashbhau101@gmail.com>
2024-06-24 13:19:41 +0530

    CodePointBefore input modified ( 24-06-2024)

--- CodePointBefore.java
+++ CodePointBefore.java
@@ -3,7 +3,7 @@ class CodePointBefore
 	static String str = new String("ABCD");
 	public static void main(String[] args) {
 		
-		int indx = userCodePointBefore(5);
+		int indx = userCodePointBefore(3);
 		System.out.println("str"+ " : "+indx);
 	}
 
