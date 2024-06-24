b3e91033b2ab6eca5bf8d0ee79508111ee5476b8
Akash Morwal <akashbhau101@gmail.com>
2024-06-24 13:04:31 +0530

    new program is updated ( 24-06-2024)

---
+++ CodePointCount.java
@@ -0,0 +1,17 @@
+class CodePointCount
+{
+	static String str = new String("ABCDEFGHI");
+	public static void main(String[] args) {
+		
+		int indx = userCodePointCount(-3,3);
+		System.out.println(str+ " : "+indx);
+	}
+	public static int userCodePointCount(int startIndx, int endIndx)
+	{
+		if(startIndx>endIndx )  //   (endIndx>0)&&(endIndx<=indx.length()) (endIndx>0)&&(endIndx<=indx.length())
+		{
+			throw new StringIndexOutOfBoundsException("Wrong index : "+"{"+startIndx+","+endIndx+"}");
+		}
+		return endIndx-startIndx;
+	}
+}
