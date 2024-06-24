0fd086b771c24dbf1d5133d08ec2b9d601b42ab5
Akash Morwal <akashbhau101@gmail.com>
2024-06-24 13:14:24 +0530

    new program of String method CodePointIntBefore Program is updated (24-06-2024)

---
+++ CodePointBefore.java
@@ -0,0 +1,18 @@
+class CodePointBefore
+{
+	static String str = new String("ABCD");
+	public static void main(String[] args) {
+		
+		int indx = userCodePointIntBefore(5);
+		System.out.println("str"+ " : "+indx);
+	}
+
+	public static int userCodePointBefore(int indx)
+	{
+		if(indx<1 || indx>str.length())
+		{
+			throw new StringIndexOutOfBoundsException("Wrong Index  :"+ indx);
+		}
+		return str.charAt(indx-1);
+	}
+}
