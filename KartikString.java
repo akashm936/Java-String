1a52f82148057b3c679d8767112428a507a41e5d
Akash Morwal <akashbhau101@gmail.com>
2024-06-28 11:20:28 +0530

    new program is updated (28-06-2024)

---
+++ KartikString.java
@@ -0,0 +1,16 @@
+class Karthik
+{
+	String gf1;
+	String gf2;
+	String gf3;
+
+	Karthik(String gf1,String gf2, String gf3)
+	{
+		this.gf1=gf1;
+		this.gf2=gf2;
+		this.gf3=gf3;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
