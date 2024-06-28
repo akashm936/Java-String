04b2e759f7d233630171315973c7fcfdfbb86944
Akash Morwal <akashbhau101@gmail.com>
2024-06-28 12:35:38 +0530

    new program is updated (28-06-2024)

--- KartikString.java
+++ KartikString.java
@@ -1,3 +1,5 @@
+
+
 class Karthik
 {
 	String gf1;
@@ -13,10 +15,27 @@ class Karthik
 
 	//overriding Equals ()
 	@Override
-	public boolean equals(){
-	//remaning
+	public boolean equals(Object obj){
+		Karthik k=(Karthik)obj;  //downcasting //
+		return this.gf1==k.gf1 && this.gf2==k.gf2 && this.gf3==k.gf3;
+
 	}
+
+	Karthik(String gf1)
+	{
+		this.gf1=gf1;
+	}
+
+
 	public static void main(String[] args) {
 		
+		Karthik k1=new Karthik("Kanchana","Kamla","Kangana");
+		Karthik k2 =new Karthik("Kanchana","Kamla","Kangana");
+
+		Karthik k3 = new Karthik("Kachadi");
+		Karthik k4=new Karthik("Kachadi");
+
+		System.out.println(k1.equals(k2));   //true
+		System.out.println(k3.equals(k4));   //false
 	}
 }
